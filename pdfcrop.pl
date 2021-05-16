07fd5726512bbe0a1a8206146e0b1f0c861f80ee
Richard Smith <richard@ex-parrot.com>
2021-05-16 10:36:28 +0100

    Add --quiet option

--- pdfcrop.pl
+++ pdfcrop.pl
@@ -350,6 +350,7 @@ $::opt_version    = 0;
 $::opt_help       = 0;
 $::opt_debug      = 0;
 $::opt_verbose    = 0;
+$::opt_quiet      = 0;
 $::opt_pdftexcmd  = "pdftex";
 $::opt_xetexcmd   = "xetex";
 $::opt_luatexcmd  = "luatex";
@@ -377,6 +378,7 @@ Options:                                                       (defaults:)
   --version           print version number
   --(no)verbose       verbose printing                         ($bool[$::opt_verbose])
   --(no)debug         debug informations                       ($bool[$::opt_debug])
+  --(no)quiet         suppress routine output                  ($bool[$::opt_quiet])
   --gscmd <name>      call of ghostscript                      ($::opt_gscmd)
   --pdftex | --xetex | --luatex
                       use pdfTeX | use XeTeX | use LuaTeX      ($::opt_tex)
@@ -441,6 +443,7 @@ GetOptions(
   "version!",
   "debug!",
   "verbose!",
+  "quiet!",
   "gscmd=s",
   "pdftexcmd=s",
   "xetexcmd=s",
@@ -472,7 +475,7 @@ $::opt_verbose = 1 if $::opt_debug;
 
 @ARGV >= 1 or usage(1);
 
-print $title;
+print $title unless $::opt_quiet;
 
 print "* Restricted mode: ", ($restricted ? "enabled" : "disabled"), "\n"
         if $::opt_debug;
@@ -1255,7 +1258,7 @@ if (!rename("$tmp.pdf", $outputfile)) {
 }
 
 print "==> $page page", (($page == 1) ? "" : "s"),
-      " written on `$outputfile'.\n";
+      " written on `$outputfile'.\n" unless $::opt_quiet;
 
 $exit_code = 0;
 cleanup();
